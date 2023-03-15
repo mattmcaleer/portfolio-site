f02a1246af27b1e54668af34fa410b0a8bc50a1e
Matthew <matthew_mcaleer@hotmail.com>
2023-03-15 21:11:51 +1100

    refactored and restyled into vanilla JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+let menuToggle = document.querySelector(".toggle-psuedo-table");
+let navbar = document.querySelector(".navigation")
+let hideMenu = true;
+let menu = document.querySelector(".nav-menu");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function toggleMenu() {
+  menu.classList.toggle("nav-menu-active");
+
+  hideMenu = !hideMenu;
+
+  if (hideMenu) {
+    menuToggle.style.opacity = '.3'
+    menuToggle.style.transform = 'rotate(0deg)';
+    menuToggle.style.transition = 'transform 1s ease-out, opacity 1s';
+    setTimeout('menu.style.height = 0;', 300)
+  } else {
+    menuToggle.style.opacity = '1'
+    menuToggle.style.transform = 'rotate(405deg)';
+    menuToggle.style.transition = 'transform 1s ease-out, opacity 1s';
+    menu.style.height = '100vh'
+  };
+}
+
+menuToggle.addEventListener('click', toggleMenu)
+
+// make function that closes navbar (if its open) when page scrolled
+
+let pos1 = window.scrollY;
+let sticky = navbar.offsetTop;
+
+window.onscroll = function() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky-navbar")
+  } else {
+    navbar.classList.remove("sticky-navbar");
+  }
+
+  let pos2 = window.scrollY;
+
+  if (pos1 > pos2) {
+    return;
+  } else {
+    hideMenu = true;
+    menu.classList = "nav-menu";
+    menuToggle.style.opacity = '.3'
+    menuToggle.style.transform = 'rotate(0deg)';
+    menuToggle.style.transition = 'transform 1s ease-out, opacity 1s';
+    setTimeout('menu.style.height = 0;', 300)
+  }
+}
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
